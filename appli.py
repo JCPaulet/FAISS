7561139dcd21f9066d2b59395b9060308a29eb20
JCPaulet <jcpanalytics@gmail.com>
2024-12-09 15:52:04 +0100

    Update appli.py

--- appli.py
+++ appli.py
@@ -173,8 +173,8 @@ if any(collection.name == collection_name for collection in collections):
     # Inspect the content
     
     print(f"Document {52}: {items['documents'][52]}")
-    print(f"Metadata {52}: {items['embeddings'][52]}")
-    #print(f"Metadata {52}: {items['metadatas'][52]}")
+    #print(f"Metadata {52}: {items['embeddings'][52]}")
+    print(f"Metadata {52}: {items['metadatas'][52]}")
     
     print("-" * 80)
 
